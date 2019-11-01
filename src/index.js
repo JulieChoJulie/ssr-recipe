f7b4d3b69e10381fd5503a6a2b0cb6ad8c2a1cf9
JulieChoJulie <h32cho@gmail.com>
2019-11-01 14:13:26 -0400

    saga middleware for getUserById

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import rootReducer from './modules';
+import rootReducer, { rootSaga } from './modules';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
     rootReducer,
     window.__PRELOADED_STATE__, // use this state as initialState
-    applyMiddleware(thunk));
+    applyMiddleware(thunk, sagaMiddleware));
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
     <Provider store={store}>
--- src/modules/users.js
+++ src/modules/users.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { call, put, takeEvery } from 'redux-saga/effects';
 
 const GET_USERS_PENDING = 'users/GET_USERS_PENDING';
 const GET_USERS_SUCCESS = 'users/GET_USERS_SUCCESS';
@@ -12,6 +13,14 @@ const getUsersFailure = payload => ({
     payload
 });
 
+const GET_USER = 'users/GET_USER';
+const GET_USER_SUCCESS = 'users/GET_USER_SUCCESS';
+const GET_USER_FAILURE = 'users/GET_USER_FAILURE';
+
+export const getUser = id => ({ type: GET_USER, payload: id });
+const getUserSuccess = data => ({ type: GET_USER, payload: data });
+const getUserFailure = error => ({ type: GET_USER_FAILURE, payload: error, error: true})
+
 export const getUsers = () => async dispatch => {
     try {
         dispatch(getUsersPending());
@@ -24,6 +33,22 @@ export const getUsers = () => async dispatch => {
         throw e;
     }
 };
+
+const getUserById = id =>
+    axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+function* getUserSaga(action) {
+    try {
+        const response = yield call(getUserById, action.payload);
+        yield put(getUserSuccess(response.data));
+    } catch (e) {
+        yield put(getUserFailure(e));
+    }
+};
+
+export function* userSaga() {
+    yield takeEver(GET_USER, getUserSaga);
+}
 
 const initialState = {
     users: null,
@@ -46,6 +71,39 @@ function users(state=initialState, action) {
             return {...state, users: action.payload.data, loading: {...state.loading, users: false}};
         case GET_USERS_FAILURE:
             return {...state, error: {...state.error, users: action.payload}, loading: {...state.loading, users: false}};
+        case GET_USER:
+            return {
+                ...state,
+                loading: {
+                    ...state.loading,
+                    user: true
+                },
+                error: {
+                    ...state.error,
+                    user: null
+                }
+            };
+        case GET_USER_SUCCESS:
+            return {
+                ...state,
+                user: action,payload,
+                loading: {
+                    ...state.loading,
+                    user: false,
+                }
+            };
+        case GET_USER_FAILURE:
+            return {
+                ...state,
+                error: {
+                    ...state.error,
+                    user: action.payload
+                },
+                loading: {
+                    ...state.loading,
+                    user: false
+                }
+            };
         default:
             return state;
     }
